19c8dbb7b05221032d671ca7e0bc1a49e7a4c78e
K-M-Shehan <miweshshehan@gmail.com>
2024-03-03 11:23:39 +0530

    comments added

--- code.ino
+++ code.ino
@@ -1,3 +1,4 @@
+// Buzzer 1 and PIR 1 is 1 unit and the same goes for the other 2
 const int buzzer1 = 10;
 const int buzzer2 = 9;
 const int pir1 = 8;
@@ -6,6 +7,7 @@ const int pir2 = 7;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  // Set PIR 1 and 2 as input and buzzers as output
   pinMode(pir1, INPUT);
   pinMode(pir2, INPUT);
   pinMode(buzzer1, OUTPUT);
@@ -15,7 +17,8 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.print("PIR Sensor Values: ");
-  
+  // the below function prints a msg in the serial monitor if motion is sensed
+  // the soundBuzzer function is what makes the buzzer make noises
   senseMotion(pir1, buzzer1);
   senseMotion(pir2, buzzer2);
   delay(500);
