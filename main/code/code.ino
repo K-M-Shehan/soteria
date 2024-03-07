93469c8c15fb7a0ce69e4b5e93aa4d6272fcb817
G0602 <gowrishan0602@gmail.com>
2024-03-07 14:22:24 +0530

    cleaned the code a littile bibit and changed the code to fit to esp 01

--- main/code/code.ino
+++ main/code/code.ino
@@ -1,29 +1,14 @@
-// Buzzer 1 and PIR 1 is 1 unit and the same goes for the other 2
-const int buzzer1 = 10;
-const int buzzer2 = 9;
-const int pir1 = 8;
-const int pir2 = 7;
+/*
+This file is for the ESP 01  whifi module to identifi an intruder
+and to sound the buzzer to warn the intruder and to inform the user about the user about
+the intruder via sounding the buzzer
+*/
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  // Set PIR 1 and 2 as input and buzzers as output
-  pinMode(pir1, INPUT);
-  pinMode(pir2, INPUT);
-  pinMode(buzzer1, OUTPUT);
-  pinMode(buzzer2, OUTPUT);
-}
- 
-void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.print("PIR Sensor Values: ");
-  // the below function prints a msg in the serial monitor if motion is sensed
-  // the soundBuzzer function is what makes the buzzer make noises
-  senseMotion(pir1, buzzer1);
-  senseMotion(pir2, buzzer2);
-  delay(500);
-}
+// Declaring GPIO 0 and GPIO 2 as buzzer and pir pins
+const int buzzer = 0;
+const int pir = 2;
 
+// declaring senseMotion function
 void senseMotion(int pir, int buzzer) {
   int pirValue = digitalRead(pir);
   if(pirValue == HIGH) {
@@ -31,16 +16,32 @@ void senseMotion(int pir, int buzzer) {
     Serial.println("Motion Detected!");
   }
   else {
-    noMotion(buzzer);
+    noTone(buzzer);
     Serial.println("No Motion");
   }
 }
 
+// declaring soundBuzzer function
 void soundBuzzer(int buzzer) {
-  tone(buzzer, 1000, 200);
+  tone(buzzer, 1000, 500);
   digitalWrite(buzzer, LOW);
 }
 
-void noMotion(int buzzer) {
-  noTone(buzzer);
+
+//Bellow this sound setup and  main loop is declared.
+void setup() {
+  Serial.begin(9600);
+  // Set pir as input and buzzer as output
+  pinMode(pir, INPUT);
+  pinMode(buzzer, OUTPUT);
 }
+ 
+void loop() {
+  Serial.print("PIR Sensor Values: ");
+  // the below function prints a msg in the serial monitor if motion is sensed
+  // then soundBuzzer function is what makes the buzzer make noises
+  senseMotion(pir, buzzer);
+  delay(2000);
+}
+
+
