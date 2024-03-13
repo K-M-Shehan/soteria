4531bee323cbe31fa91d4805f201ce2c51024076
G0602 <gowrishan0602@gmail.com>
2024-03-13 22:40:31 +0530

    some minor changes

--- main/ssid.h
+++ main/ssid.h
@@ -1,5 +1,5 @@
 // WiFi credentials
 #ifndef SSID
-const char ssid[] = "Just A Wifi";
+const char ssid[] = "Just A WiFi";
 const char ssid_pw[] = "00opin00";
 #endif
--- unit/code/code.ino
+++ unit/code/code.ino
@@ -7,13 +7,13 @@ the intruder via sounding the buzzer
 #include"localio.h"
 
 // Declaring GPIO 0 and GPIO 2 as buzzer and pir pins
-const int pir = 4;
-const int buzzer = 5;
+const int pir = 0;
+const int buzzer = 2;
 
 
 //Bellow this sound setup and the main loop is declared.
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   // Set pir as input and buzzer as output
   pinMode(pir, INPUT);
   pinMode(buzzer, OUTPUT);
@@ -22,5 +22,5 @@ void setup() {
 void loop() {
   Serial.print("PIR Sensor Values: ");
   senseMotion(pir, buzzer);
-  delay(500);
+  delay(1000);
 }
