18991c75146549077e58cdefe024535469a3fd04
G0602 <gowrishan0602@gmail.com>
2024-04-20 22:05:22 +0530

    added code to the subUnit to make it a TCP/IP client and added some lines to the ssid.h file

--- esp-8266-01/subUnit/subUnit.ino
+++ esp-8266-01/subUnit/subUnit.ino
@@ -4,19 +4,42 @@ and to sound the buzzer to warn the intruder and inform the user about the user 
 the intruder via sounding the buzzer
 */
 
-#include"localio.h"
+#include <ESP8266WiFi.h>
+#include "../wi_fi/ssid.h"
+#include "../wi_fi/wi_fi.h"
+#include "localio.h"
 
 // Declaring GPIO 0 and GPIO 2 as buzzer and pir pins
 const int pir = 0;
 const int buzzer = 2;
 
+//setting this esp-01 as the client
+WiFiClient client;
 
 //Bellow this sound setup and the main loop is declared.
 void setup() {
   Serial.begin(115200);
+
   // Set pir as input and buzzer as output
   pinMode(pir, INPUT);
   pinMode(buzzer, OUTPUT);
+
+  // Connect to WiFi defined in "../wi_fi/wi_fi.ino"
+  connectToWiFi(ssid, ssid_pw);
+
+  // Connect to the server
+  Serial.print("Connecting to server: ");
+  Serial.println(serverIp);
+
+  if (client.connect(serverIp, serverPort)) {
+    /*make some kind of sound to indicate the connection is sucesfull
+    
+    Once connected, send data to the server
+    client.println("Hello from client");*/
+
+  } else {
+    //make some kind of sound to indicate the connection is not sucesfull
+  }
 }
  
 void loop() {
--- esp-8266-01/wi_fi/ssid.h
+++ esp-8266-01/wi_fi/ssid.h
@@ -3,4 +3,6 @@
 #define SSID
 const char ssid[] = "Just A WiFi";
 const char ssid_pw[] = "00opin00";
+const int serverPort = 80;
+const int serverIp; //need to find out
 #endif
