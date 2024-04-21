ea3db578708a80fdb8cdfcce569acba649da819d
K-M-Shehan <miweshshehan@gmail.com>
2024-04-21 10:11:22 +0530

    added TODO

--- esp-8266-01/subUnit/localio.ino
+++ esp-8266-01/subUnit/localio.ino
@@ -9,6 +9,10 @@ void soundBuzzer(int buzzer) {
 }
 
 // declaring senseMotion function for pir sensor
+// declaring senseMotion function for pir sensor
+// TODO: move noTone from sense motion when connecting to app
+// the reason we have to do this is cause we have to get user input to sound the buzzer
+// in senseMotion() the buzzer would sound as soon as motion is detected this output is not what we desire
 void senseMotion(int pir, int buzzer) {
   int pirValue = digitalRead(pir);
   if(pirValue == HIGH) {
