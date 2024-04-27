1f47642d10603567d6d3f6bd0631b34effdea36c
G0602 <gowrishan0602@gmail.com>
2024-04-27 18:58:00 +0530

    removed the haeder file because it is not nessesarry for the .ino file

--- esp8266-01/subUnit/localio.h
+++
@@ -1,8 +0,0 @@
-// Function prototypes defined in localio.ino file
-#ifndef LOCALIO
-#define LOCALIO
-
-void soundBuzzer(int buzzer);
-void senseMotion(int pir, int buzzer);
-
-#endif
--- esp8266-01/subUnit/localio.ino
+++ esp8266-01/subUnit/localio.ino
@@ -1,14 +1,11 @@
 //This file contains all the functions mentionend in the local.h header file
 
-#include"localio.h"
-
 // declaring soundBuzzer function for the buzzer module
 void soundBuzzer(int buzzer) {
   tone(buzzer, 1000, 500);
   delay(1000);
 }
 
-// declaring senseMotion function for pir sensor
 // declaring senseMotion function for pir sensor
 // TODO: move noTone from sense motion when connecting to app
 // the reason we have to do this is cause we have to get user input to sound the buzzer
