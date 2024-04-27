63be32211ff6a3dcc4f5855c952af191585d44fb
G0602 <gowrishan0602@gmail.com>
2024-04-28 00:55:28 +0530

    created seperate server via seperate port to communicate with the app and started doin the logic for that still have alot to do

--- esp8266-01/mainUnit/mainUnit.ino
+++ esp8266-01/mainUnit/mainUnit.ino
@@ -1,16 +1,13 @@
 #include <ESP8266WiFi.h>
 #include "/media/g0602/New Volume/SLIIT_DOCs/Y1S1/Computational Thinking/soteria/esp8266-01/wi_fi/ssid.h" //you need to add the absolute link of this file here
 
-WiFiServer server(serverPort);
-WiFiClient clients[maxClients]; // Array to store client objects
-bool clientActive[maxClients] = {false}; // Array to track active clients
+WiFiServer unit_server(unitPort); // port to connect with the sub units
+WiFiServer app_server(appPort); // port to connect with the app
 
-typedef struct{
-  int input;
-  int output;
-} Sub; // a structure to send and recive information with subunit
+WiFiClient clients[maxUnits]; // Array to store client objects
+bool clientActive[maxUnits] = {false}; // Array to track active clients
 
-Sub sub[maxClients - 1];// an array of Sub class to trnsfer data
+bool status = 1; //This indicate whether to sound the buzzer or not
 
 void setup() 
 {
@@ -23,18 +20,38 @@ void setup()
 
 
 	// Start the server
-  server.begin();
+  unit_server.begin();
+  app_server.begin();
   Serial.println("Server started");
 }
 
 void loop() 
 {
-	WiFiClient newClient = server.available();
+  WiFiClient app = app_server.available();
 
-	//following code only run if the client is available
-   if (newClient) {
+  if (app.connected()) {
+    if (app.available()) {
+      String command = app.readStringUntil('\r');
+
+      switch(command[1]){
+        case 1: break;
+        case 2: break;
+        case 3: break;
+        case 4: break;
+        case 5: break;
+        case 6: break;
+        case 7: break;
+        default : break;
+      }
+    }
+  }
+
+	WiFiClient newClient = unit_server.available();
+
+	//following code only run if any new client is available on unitPort
+  if (newClient) {
     // Find an empty slot in the clients array
-    for (int i = 0; i < maxClients; i++) {
+    for (int i = 0; i < maxUnits; i++) {
       if (!clientActive[i]) {
         clients[i] = newClient;
         clientActive[i] = true;
@@ -45,7 +62,7 @@ void loop()
   }
 
   // Handle each connected client
-  for (int i = 0; i < maxClients; i++) {
+  for (int i = 0; i < maxUnits; i++) {
     if (clientActive[i]) {
       if (clients[i].connected()) {
         if (clients[i].available()) {
@@ -67,81 +84,4 @@ void loop()
       }
     }
   }
-
-	/*
-  // Check for human presence
-  if (detectHumanPresence()) 
-  {
-    // Alert user
-    sendAlertToPhone();
-    
-    // User decision
-    bool soundBuzzer = getUserDecision();
-
-    // Sound the buzzer if required
-    if (soundBuzzer) 
-    {
-      activateBuzzer();
-      
-      // Monitor human presence and check if user wants to escalate
-      if (!monitorHumanPresence()) 
-      {
-        promptToCallPolice();
-      }
-
-      // Deactivate the buzzer
-      deactivateBuzzer();
-    }
-  }*/
 }
-
-
-/*
-bool detectHumanPresence() 
-{
-  // this code detects human presence using the PIR sensor
-  // Return true if human is detected, false otherwise
-  int pirVal= digitalRead(pirSensorPin);
-  if(pirVal == HIGH) 
-  {
-    Serial.println("Motion Detected!");
-    return true;
-  }
-  else 
-  {
-    Serial.println("No Motion");
-    return false;
-  }
-}
-
-void sendAlertToPhone() 
-{
-  // code to send an alert to the user's phone
-}
-
-bool getUserDecision() 
-{
-  // this code interacts with the user and get their decision
-  // Return true if user wants to sound the buzzer, false otherwise
-}
-
-bool monitorHumanPresence() 
-{
-  // this code monitors human presence and return true if human is still present after 20 seconds
-  if (detectHumanPresence()  == true)
-  {
-    delay(20000);
-    if (detectHumanPresence()  == true)
-      return true;
-    else
-      return false;
-  } 
-  else
-    return false;
-}
-
-void promptToCallPolice() 
-{
-  // this code sends a prompt to the user's phone asking to call the police
-}
-*/
