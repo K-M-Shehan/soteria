d1805730d618068d3d268341ed0418fb80ffe86f
G0602 <gowrishan0602@gmail.com>
2024-04-28 00:53:41 +0530

    added another port number to connect with the app

--- esp8266-01/wi_fi/ssid.h
+++ esp8266-01/wi_fi/ssid.h
@@ -3,7 +3,8 @@
 #define SSID
 const char ssid[] = "Just A WiFi";
 const char ssid_pw[] = "00opin00";
-const int serverPort = 80;
+const int appPort = 80;
+const int unitPort = 8080;
 const char serverIp[] = "192.168.193.141";
-const int maxClients = 3; 
+const int maxUnits = 2; 
 #endif
