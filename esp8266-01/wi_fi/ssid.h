28dc0570e9cdc04b40e8c4dfe852460876c952aa
Aathifilahy <aathifilah@gmail.com>
2024-04-28 12:42:58 +0530

    modifide the app

--- esp8266-01/wi_fi/ssid.h
+++ esp8266-01/wi_fi/ssid.h
@@ -3,8 +3,8 @@
 #define SSID
 const char ssid[] = "Just A WiFi";
 const char ssid_pw[] = "00opin00";
-const int appPort = 80;
+const int appPort = 1050;
 const int unitPort = 8080;
-const char serverIp[] = "192.168.193.141";
+const char serverIp[] = "192.168.193.77";
 const int maxUnits = 2; 
 #endif
